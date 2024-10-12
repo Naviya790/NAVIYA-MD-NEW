4dd5c366f8a0f1b37fee87b19b5279ed11e4ee7a
accnaviy <gnaviya79@gmail.com>
2024-10-12 06:51:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gvdAQAiL#R4gH-xwYlM8uepgbzd8b58QbTBfHXWaXT44cU6PNIcs",
+SESSION_ID: process.env.SESSION_ID || "0ncQwara#zhyJfv2p7uJYW19_Y2rlTqpJL1CDw9K6vM8BRbV13Ms",
 MONGODB: process.env.MONGODB || "mongodb://mongo:nCNtvmpZQfbmQyteQXzHtYWyzpjMSpcp@autorack.proxy.rlwy.net:55349",
 };
 
