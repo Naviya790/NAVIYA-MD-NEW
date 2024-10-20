d49c2307cb1d4bec8d2b2c83303fcd6fc8807aa0
accnaviy <gnaviya79@gmail.com>
2024-10-20 14:36:22 +0530

    Update config.js
    NAVIYA MD ⚡

--- config.js
+++ config.js
@@ -8,3 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "3K5khaKD#D4jqtd1W52WWleS2ruGMTUFcBZ_7j8y7ibtYaiite0M",
 MONGODB: process.env.MONGODB || "mongodb://mongo:rrQTfFWvxVSIXOYVmumTFKuTkahWbaJS@junction.proxy.rlwy.net:46641",
 };
+
