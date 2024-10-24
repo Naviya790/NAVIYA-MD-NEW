8521442c0fe9fa3bf6ba15b32010dc39428839de
accnaviy <gnaviya79@gmail.com>
2024-10-24 15:40:01 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -11,7 +11,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+let data = await fetchJson(`https://dark-yasiya-api-new.vercel.app/ai/gemini?q=$`)
 reply(`${data.data}`)
 }catch(e){
 console.log(e)
