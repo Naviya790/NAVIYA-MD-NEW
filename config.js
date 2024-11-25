727e21735db846a53e1985acd40a46960c16efad
Naviya <gnaviya79@gmail.com>
2024-11-25 14:59:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yFFDAYCY#HinBUxW2jSTpF1dkaxCinDxzvCnEdqZZx-LtUGU7aww",
+SESSION_ID: process.env.SESSION_ID || "ɴᴀᴠɪʏᴀ-ᴍᴅ=3dkE0K5Y#FMPUQDAKvCIY5w4P0vDZlck920dXcAei3zzq0d1nZg8",
 MONGODB: process.env.MONGODB || "mongodb://mongo:BklkgmkJmnXvnlxLSChccDiHGGvWHylD@autorack.proxy.rlwy.net:25578",
 };
 
